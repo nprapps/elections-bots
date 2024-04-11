3b9339e450fafa03cb676c94f60f1825da43456d
Shajia Abidi <sabidi@npr.org>
2024-04-11 16:44:44 -0700

    feat: get calendar data from AP API

--- elex-testing-results/api.js
+++ elex-testing-results/api.js
@@ -1,1 +1,22 @@
-//This is a test
+var axios = require("axios");
+
+const URL = "https://api.ap.org/v3/reports/Calendar-CustomerTesting2024-Live?format=json"
+const headers = {"x-api-key": process.env.AP_API_KEY};
+
+//this will need to be updated when needed
+const calendarYear = "ElectionTestingSchedule2024"
+
+async function getElexTestData() {
+    try {
+        const response = await axios({
+            url: URL,
+            headers
+        });
+        const data = response.data
+        console.log(data[calendarYear]);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+getElexTestData()
