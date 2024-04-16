848b7fc733841380928605cee33156ec9631a3b6
Shajia Abidi <sabidi@npr.org>
2024-04-16 11:15:43 -0700

    checking gsheets

--- elex-testing-results/app.js
+++ elex-testing-results/app.js
@@ -1,7 +1,6 @@
 const { getElexTestData } = require("./getElexTestData");
 const { sendMessageToSlack } = require("./sendMessageToSlack");
-
-(async function () {
+const { getDataFromSheets } = require("./getDataFromSheets")(async function () {
   console.log("Hello! We are testing github actions");
 
   const isSheetEmpty = await getDataFromSheets();
