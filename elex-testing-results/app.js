3194485f82a5386e2c431c80c0a396591c0820d9
Shajia Abidi <sabidi@npr.org>
2024-04-16 09:48:42 -0700

    formatting fix

--- elex-testing-results/app.js
+++ elex-testing-results/app.js
@@ -1,10 +1,10 @@
 const { getDataFromSheets } = require("./getDataFromSheets");
-const { sendMessageToSlack } = require("./sendMessageToSlack")(
-  async function () {
-    console.log("Hello! We are testing github actions");
+const { sendMessageToSlack } = require("./sendMessageToSlack");
 
-    //   const isSheetEmpty = await getDataFromSheets();
-    //   console.log({ isSheetEmpty });
-    await sendMessageToSlack();
-  }
-)();
+(async function () {
+  console.log("Hello! We are testing github actions");
+
+  //   const isSheetEmpty = await getDataFromSheets();
+  //   console.log({ isSheetEmpty });
+  await sendMessageToSlack();
+})();
