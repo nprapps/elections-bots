162c360f49235cc94786550192e74c681f2eaf54
Shajia Abidi <sabidi@npr.org>
2024-04-15 19:01:50 -0700

    chore: organize code into appropriate files and have all the main functions done

---
+++ elex-testing-results/deleteDataInSheets.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+var { google } = require("googleapis");
+var login = require("@nprapps/google-login");
+
+async function deleteDataInSheets() {
+    const spreadsheetId = process.env.SHEETS_ID
+    const range = "Sheet1!A1:Z10000"
+
+    var auth = login.getClient();
+    const sheets = google.sheets({ version: 'v4', auth });
+
+    try {
+        const result = await sheets.spreadsheets.values.clear({
+            spreadsheetId,
+            range,
+        });
+        console.log("Data cleared");
+        return result;
+    } catch (err) {
+        throw err;
+    }
+}
+
+deleteDataInSheets()
+module.exports = {
+    deleteDataInSheets
+}
---
+++ elex-testing-results/writeDataToSheets.js
@@ -0,0 +1,48 @@
+require('dotenv').config()
+var { google } = require("googleapis");
+var login = require("@nprapps/google-login");
+const { updateDateInSheets } = require("./updateDateInSheets");
+const { sendMessageToSlack } = require("./sendMessageToSlack")
+
+async function writeDataToSheets(lastUpdatedDate, values) {
+    const spreadsheetId = process.env.SHEETS_ID
+    const range = "Sheet1!A4:Z10000"
+    const valueInputOption = "USER_ENTERED"
+
+    await updateDateInSheets(lastUpdatedDate)
+
+    var auth = login.getClient();
+    const sheets = google.sheets({ version: 'v4', auth });
+
+    try {
+        
+        const result = await sheets.spreadsheets.values.append({
+            spreadsheetId,
+            range,
+            valueInputOption,
+            resource: { values }
+        });
+
+        console.log('%d cells updated.', result.data.updates.updatedCells);
+
+
+        //! Check if there is any upcoming test data for today
+        // if(upcomingTests){
+        //     start the cron job for today
+        //     
+        //     send slack message accordingly
+        // } else {
+        //     do nothing
+        // }
+        // await sendMessageToSlack()
+
+        return result;
+    } catch (err) {
+        throw err;
+    }
+}
+
+
+module.exports = {
+    writeDataToSheets
+}
