e276bd57e708a2e689943bc0a10ca1c4e9fe37cc
Shajia Abidi <sabidi@npr.org>
2024-04-26 12:27:13 -0700

    more utc time

--- elex-testing-results/helpers/compareTime.js
+++ elex-testing-results/helpers/compareTime.js
@@ -9,13 +9,13 @@ async function compareTime(elexTestData) {
         "2024-04-26",
         "PR Dem Presidential Primary",
         "Customer Testing",
-        "12:00 pm-7:00 pm",
+        "3:30 pm-7:00 pm",
       ],
       [
         "2024-04-26",
         "NY CD 26 Special Election",
         "Customer Testing",
-        "8:00 am-7:30 pm",
+        "4:00 am-7:30 pm",
       ],
       [
         "2024-04-26",
@@ -27,7 +27,7 @@ async function compareTime(elexTestData) {
         "2024-04-26",
         "NY CD 26 Special Election",
         "Customer Testing",
-        "2:10 pm-7:30 pm",
+        "12:10 pm-7:30 pm",
       ],
       [
         "2024-04-26",
@@ -44,32 +44,39 @@ async function compareTime(elexTestData) {
   console.log({ todaysTests });
 
   const currentTime = new Date();
-  const currentTimeEST = new Date().toLocaleTimeString("en-US", {
-    timeZone: "America/New_York",
-  });
+  const currentTimeUTC = new Date("2024-04-26 4:00 pm").toLocaleTimeString(
+    "en-US",
+    {
+      timeZone: "America/New_York",
+    }
+  );
 
-  console.log({ currentTime });
-  console.log({ currentTimeEST });
+  // console.log({ currentTime });
+  // console.log({ currentTimeUTC });
 
   const messagesToSend = [];
 
   todaysTests.map((data) => {
     const date = data[0];
     const time = data[3];
+
+    console.log({ time });
     if (time) {
+      //! don't hard code pm value
       const startTestingTime = `${time.slice(0, 5)} pm`;
 
+      //! add 4 hours to make it to UTC
       let testingTime = new Date(`${date} ${startTestingTime}`);
 
       const timeDiff = diff_minutes(currentTime, testingTime);
 
-      console.log({ startTestingTime });
-      console.log({ testingTime });
+      // console.log({ startTestingTime });
+      // console.log({ testingTime });
 
       console.log({ timeDiff });
       console.log("-------");
 
-      if (timeDiff > 240 && timeDiff <= 280) {
+      if (timeDiff > -240 && timeDiff <= -280) {
         messagesToSend.push(data);
       }
     }
--- elex-testing-results/helpers/diffMinutes.js
+++ elex-testing-results/helpers/diffMinutes.js
@@ -1,13 +1,23 @@
 //Got this function from https://www.w3resource.com/javascript-exercises/javascript-date-exercise-44.php
 
 function diff_minutes(currentTime, testingTime) {
+  console.log({ currentTime });
+  console.log({ testingTime });
+
   // Calculate the difference in milliseconds between the two provided dates and convert it to seconds
-  var diff = (testingTime.getTime() - currentTime.getTime()) / 1000;
+
+  var diff = (currentTime.getTime() - testingTime.getTime()) / 1000;
   // Convert the difference from seconds to minutes
   diff /= 60;
   // Return the absolute value of the rounded difference in minutes
+
+  console.log(Math.round(diff));
   return Math.round(diff);
 }
+// const a = new Date("2024-04-26 3:00 pm");
+// const b = new Date("2024-04-26 4:20 pm");
+
+// diff_minutes(a, b);
 
 module.exports = {
   diff_minutes,
